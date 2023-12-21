83906554828c916570610f381d248bf5b85e0219
chirayu humar <chirayuhumar@gmail.com>
2023-12-22 03:51:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const track = document.querySelector(".carousel_track");
+const slides = Array.from(track.children);
+const nextButton = document.querySelector(".carousel_btn_left");
+const prevButton = document.querySelector(".carousel_btn_right");
+
+const dotsNav
